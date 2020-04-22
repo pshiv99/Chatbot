f2de6fdf6e5934810fa6cc02a3cb67a44c3c7636
Prashant <prashantshivhare81@gmail.com>
2020-04-22 21:43:25 +0530

    implemented get request

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/webhook')
+verify_token = 'abcd'
+
+@app.route('/webhook', methods = ['GET'])
 def index():
-    return 'Prashant Shivhare'
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+    
+    if mode and token:
+        if mode == 'subscribe' and token == verify_token:
+            print('WEBHOOK_VERIFIED')
+            return challenge
